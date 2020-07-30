48a05d32bb0c48f4323e8b65b4516a922a651db2
lyqht <estee.tsw@gmail.com>
2020-07-30 16:12:38 +0800

    added schedule_test.py and test json files. moved activity_levels.py over to NUC folder.

--- NUC/activity_levels_test.py
+++ NUC/activity_levels_test.py
@@ -1,17 +1,13 @@
 from activity_levels import get_activity_levels, stitch_data
-from file_utils import load_json
 
 def test_stitch_data():
     test_list = [{'0907': ['a', 'b', 'c'], '0906': ['d', 'e', 'f'], '0910': ['g', 'h', 'i']}, {'0901': ['j', 'k', 'l'], '0902': ['m', 'n', 'o']}, {'0904': ['p', 'q', 'r']}]
     print(stitch_data(test_list))
 
 def test_activity_levels():
-<<<<<<< HEAD
     data_path = "./sample_activity_levels/new_format/2020.07.16.json"
-=======
-    data_path = 'sample_activity_levels/formatted_history/2020.07.14.json'
->>>>>>> parent of 5bea12b... Formatted 24 hour activity levels
-    data = load_json(data_path)
+    with open(data_path) as json_file:
+        data = json.load(json_file)
     get_activity_levels(data, debug=True)
         
-test_activity_levels()
+test_stitch_data()
