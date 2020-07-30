f014907956b4b356cb22899c54ba68cd6d71624e
SolsticeDante <solsticedante@gmail.com>
2020-07-30 20:24:05 +0800

    send notifications when a person is in a room longer than they're supposed to (incomplete feature)

--- NUC/__main__.py
+++ NUC/__main__.py
@@ -3,8 +3,12 @@ from   datetime import datetime
 import paho.mqtt.client as mqtt
 import serial
 import numpy as np
+import math
+import pdb
 
 from RoomMonitor import RoomMonitor, LIVING_ROOM, BED_ROOM, KITCHEN, TOILET, OUTSIDE
+
+
 
 def bitfield(n):
     return [1 if digit=='1' else 0 for digit in bin(n)[2:]]
@@ -71,45 +75,60 @@ def send_msg_if_room_changed(state_value: int, last: str, topic: str, state_mach
     return last, True
 
 def on_message(client,userdata, msg):
-    topic=msg.topic
-    
-    # print("=============================") # debug message
-    # print("message received!")
-    m_decode=str(msg.payload.decode("utf-8","ignore"))
-    # print("msg: {0}".format(m_decode))
-    
-    device_type, house_id, room_type = topic.split("/")
-    # print("Device Type: {}, House_ID: {}, Room_Type: {}".format(device_type, house_id, room_type))
+    global last_entered_time
+    global x
+    global last_visited
+    global topic
+    global state_machine
+    pdb.set_trace()
 
-
-    if m_decode == "0" or m_decode == "1":
-        binary_dict[room_type] = int(m_decode)
-        state_value = 0
-        for x in weight_dict:
-            state_value += weight_dict[x] * binary_dict[x]
-        if isPowerOfTwo(state_value):
-            for x in binary_dict:
-                if x == room_type:
-                    binary_dict[x] = 1
-                else:
-                    binary_dict[x] = 0
-        print(binary_dict)
-    print("=============================")
-    
-    val = 0
-    print(binary_dict)
-    for room in weight_arr:
-        val = val + int(binary_dict[room])*int(weight_dict[room])
-    
     try:
+        topic=msg.topic
+        
+        # print("=============================") # debug message
+        # print("message received!")
+        m_decode=str(msg.payload.decode("utf-8","ignore"))
+        # print("msg: {0}".format(m_decode))
+        
+        device_type, house_id, room_type = topic.split("/")
+        # print("Device Type: {}, House_ID: {}, Room_Type: {}".format(device_type, house_id, room_type))
+
+
+        if m_decode == "0" or m_decode == "1":
+            binary_dict[room_type] = int(m_decode)
+            state_value = 0
+            for x in weight_dict:
+                state_value += weight_dict[x] * binary_dict[x]
+            if isPowerOfTwo(state_value):
+                for x in binary_dict:
+                    if x == room_type:
+                        binary_dict[x] = 1
+                    else:
+                        binary_dict[x] = 0
+            # print(binary_dict)
+        # print("=============================")
+    except ValueError:
+        print("Invalid string0")
+    try:    
+        val = 0
+        # print(binary_dict)
+        for room in weight_arr:
+            val = val + int(binary_dict[room])*int(weight_dict[room])
+        
         last_visited, room_changed = send_msg_if_room_changed(x, last_visited, topic, state_machine)
+        print("room_changed: {0}".format(room_changed))
+        if room_changed:
+            last_entered_time = datetime.now()
+            
         # print(state_machine.current_state.name)
     except ValueError:
-        print("Invalid string")
+        print("Invalid string1")
+    except Exception as e:
+        print(e)
+        exit(0)
 
 # MQTT Setup
-client_name = "swcannotconnecthalp" 
-client = mqtt.Client(client_name)
+client = mqtt.Client()
 
 # Connect to broker
 BROKER = '192.168.0.102' 
@@ -142,6 +161,7 @@ client.publish("/".join([DEVICE_TYPE, HOUSE_ID, topic]), "Started NUC!")
 state_machine  = RoomMonitor()
 print("original state :", state_machine.current_state)
 initial_room = state_machine.current_state.name
+global last_visited
 last_visited = initial_room
 
 print("Monitoring Presence...")
@@ -149,5 +169,21 @@ weight_arr = [BED_ROOM, LIVING_ROOM, KITCHEN, OUTSIDE, TOILET]
 weight_dict = {weight_arr[i] : 2**i for i in range(5)} 
 binary_dict = {weight_arr[i] : 0 for i in range(5)}
 
+# if the person has stayed in a particular room for more than the cutoff time for that room, a notification is sent to the MQTT
+ROOM_CUTOFF_TIME_MINUTES = {
+    LIVING_ROOM: 60,
+    BED_ROOM: 60,
+    OUTSIDE: 60,
+    TOILET: 60,
+    KITCHEN: 60
+}
+last_entered_time = datetime.now()
+
 while True:
-    print(binary_dict)
+    # print("binary_dict: {0}; last_entered_time: {1}".format(binary_dict, last_entered_time))
+    cutoff_duration_minutes = ROOM_CUTOFF_TIME_MINUTES[last_visited]
+    current_duration = datetime.now()-last_entered_time
+    # print("binary_dict: {0}; current_duration(seconds): {1}; cutoff_duration_minutes: {2}".format(binary_dict, current_duration, cutoff_duration_minutes))
+    if current_duration.total_seconds() >= cutoff_duration_minutes * 60:
+        print("PONG")
+        exit(0)
